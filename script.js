c6db638b01d5063256eefa587aeacb52ae12797c
Abhilash-gowdaa <abhilashgowda248@gmail.com>
2026-05-18 18:27:26 +0530

    Campus Voice DevOps Project

---
+++ script.js
@@ -0,0 +1,12 @@
+function login(){
+
+window.location.href = "dashboard.html";
+}
+
+function submitFeedback(){
+
+document
+.getElementById("msg")
+.innerHTML =
+"Feedback Submitted Successfully 🎉";
+}
